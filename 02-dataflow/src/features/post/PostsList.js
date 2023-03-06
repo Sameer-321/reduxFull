faaec8cb10e21531539ef9ec2c029923ea12d206
Sameer-321 <snrsameer1@gmail.com>
2023-03-06 11:11:44 +0545

    makingPost-Author-Form-inRedux

--- 02-dataflow/src/features/post/PostsList.js
+++ 02-dataflow/src/features/post/PostsList.js
@@ -1,22 +1,36 @@
 import { useSelector } from "react-redux";
 import React from "react";
 import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
 
 function PostsList() {
   const posts = useSelector(selectAllPosts);
 
-  const renderedPosts = posts.map((i) => {
+  const renderedPosts = posts.map((i) => 
     // console.log(i.title);
     <article key={i.id}>
       <h3>{i.title}</h3>
       <p>{i.content.substring(0, 100)}</p>
-    </article>;
-  });
+      <p className="postCredit">
+        <PostAuthor userId={i.userId} />
+      </p>
+    </article>
+  );
+  const show = () => {
+    return(
+      <div>adfasdf</div>
+    )
+    
+  };
   return (
-    <section>
-      <h2>Posts</h2>
-      {renderedPosts}
-    </section>
+    <>
+      <section>
+        <h2>Posts</h2>
+        {renderedPosts}
+
+      </section>
+      
+    </>
   );
 }
 
--- 02-dataflow/src/features/post/postsSlice.js
+++ 02-dataflow/src/features/post/postsSlice.js
@@ -1,23 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice,nanoid } from "@reduxjs/toolkit";
 
 const initialState = [
-    {
-        id: "1",
-        title: "Machine Learining",
-        content: "asdkfjg alkh asdf hlhdlhasd",
-      },
+  {
+    id: "1",
+    title: "Machine Learining",
+    content: "asdkfjg alkh asdf hlhdlhasd",
+  },
   { id: "2", title: "Web Development", content: "mjh,nkbuygnkubg " },
 ];
 const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    postAdded(state,action){
-      state.push(action.payload)
-    }
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content,userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            userId
+          },
+        };
+      },
+    },
   },
 });
 
 export const selectAllPosts = (state) => state.posts;
-export const {postAdded}=postsSlice.actions
+export const { postAdded } = postsSlice.actions;
 export default postsSlice.reducer;
