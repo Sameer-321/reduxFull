34815d90ba99885b145d74dcad38dd6a2d4f5257
Sameer-321 <snrsameer1@gmail.com>
2023-03-06 13:29:46 +0545

    addingTimeAndEmoji:ComponentAndFucntion

--- 02-dataflow/src/features/post/PostsList.js
+++ 02-dataflow/src/features/post/PostsList.js
@@ -2,26 +2,25 @@ import { useSelector } from "react-redux";
 import React from "react";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
-
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
+//import ReactionButtons from "./ReactionButtons";
 function PostsList() {
   const posts = useSelector(selectAllPosts);
-
-  const renderedPosts = posts.map((i) => 
+  const orderedPosts = posts.slice().sort((a,b)=>b.date.localeCompare(a.date))
+  const renderedPosts = orderedPosts.map((i) => 
     // console.log(i.title);
     <article key={i.id}>
       <h3>{i.title}</h3>
       <p>{i.content.substring(0, 100)}</p>
       <p className="postCredit">
         <PostAuthor userId={i.userId} />
+        <TimeAgo timestamp={i.date}/>
       </p>
+      <ReactionButtons post={i}/>
     </article>
   );
-  const show = () => {
-    return(
-      <div>adfasdf</div>
-    )
-    
-  };
+  
   return (
     <>
       <section>
--- 02-dataflow/src/features/post/postsSlice.js
+++ 02-dataflow/src/features/post/postsSlice.js
@@ -1,12 +1,32 @@
-import { createSlice,nanoid } from "@reduxjs/toolkit";
-
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 const initialState = [
   {
     id: "1",
     title: "Machine Learining",
     content: "asdkfjg alkh asdf hlhdlhasd",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumpsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
-  { id: "2", title: "Web Development", content: "mjh,nkbuygnkubg " },
+  {
+    id: "2",
+    title: "Web Development",
+    content: "mjh,nkbuygnkubg ",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumpsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
 ];
 const postsSlice = createSlice({
   name: "posts",
@@ -16,20 +36,35 @@ const postsSlice = createSlice({
       reducer(state, action) {
         state.push(action.payload);
       },
-      prepare(title, content,userId) {
+      prepare(title, content, userId) {
         return {
           payload: {
             id: nanoid(),
             title,
             content,
-            userId
+            date: new Date().toISOString(),
+            userId,
+            reactions:{
+              thumpsUp:0,
+              wow:0,
+              heart:0,
+              rocket:0,
+              coffee:0
+            }
           },
         };
       },
     },
+    reactionAdded(state,action){
+      const {postId,reaction}= action.payload
+      const existingPost = state.find(post=>post.id===postId)
+      if(existingPost){
+        existingPost.reactions[reaction]++
+      }
+    }
   },
 });
 
 export const selectAllPosts = (state) => state.posts;
-export const { postAdded } = postsSlice.actions;
+export const { postAdded,reactionAdded } = postsSlice.actions;
 export default postsSlice.reducer;
